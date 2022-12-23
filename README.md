78f84861a37ded52c38232efd35321744095a21e
Brian Li <brnli7@gmail.com>
2022-12-23 11:32:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Example usage
+
+```
+yarn
+yarn start
+```
+
---
+++ src/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const RomulusDelegateABI = require("../abis/RomulusDelegate.json");
+
+const { newKit } = require("@celo/contractkit");
+
+const delegateAddr = "0xa7581d8E26007f4D2374507736327f5b46Dd6bA8";
+
+const kit = newKit("https://forno.celo.org");
+const delegate = new kit.web3.eth.Contract(RomulusDelegateABI, delegateAddr);
+
+const { EXPECTED_PROPOSALS } = process.env;
+
+if (EXPECTED_PROPOSALS === undefined) {
+  throw new Error("EXPECTED_PROPOSALS is not defined.");
+}
+const main = async () => {
+  const proposalCount = parseInt(await delegate.methods.proposalCount().call());
+  if (proposalCount !== parseInt(EXPECTED_PROPOSALS)) {
+    throw new Error(
+      `Expected ${EXPECTED_PROPOSALS} proposal(s), but got ${proposalCount} proposal(s).`
+    );
+  } else {
+    console.log(
+      `Expected ${EXPECTED_PROPOSALS} proposal(s), and got ${proposalCount} proposal(s).`
+    );
+  }
+};
+
+main();
